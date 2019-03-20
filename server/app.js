5319b111b75faa9765cf358ce9d4091052807c32
Jordan Machalek <jdm4344@rit.edu>
2019-03-19 23:19:54 -0400

    Fixed location of disable x-powered-by

--- server/app.js
+++ server/app.js
@@ -42,7 +42,6 @@ const app = express();
 
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
@@ -64,6 +63,7 @@ app.use(session({
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
+app.disable('x-powered-by');
 app.use(cookieParser());
 
 // csrf must come AFTER app.use(cookieParser());
